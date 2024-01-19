1cffa30a6d5afa25226951fa8808fa00caf45ace
PranauvShanmuganathan <pranavsurya234@gmail.com>
2024-01-19 13:00:00 +0530

    Menu table DDL add-ons

--- ddl/menu.sql
+++ ddl/menu.sql
@@ -3,4 +3,9 @@ create or replace table menu (
     restaurant_id text ,
     item_name text,               
     description text,             
-    price text );                
+    price text ,
+    category text,                 
+    availability text,             
+    item_type text,                
+    created_date text,             
+    modified_date text );
