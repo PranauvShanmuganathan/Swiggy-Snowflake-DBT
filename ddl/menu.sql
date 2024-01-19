66049fe84ebfa07eb7263ff5fcae50c9ad18840e
PranauvShanmuganathan <pranavsurya234@gmail.com>
2024-01-19 13:00:00 +0530

    Menu table

---
+++ ddl/menu.sql
@@ -0,0 +1,6 @@
+create or replace table menu (
+    menu_id text ,
+    restaurant_id text ,
+    item_name text,               
+    description text,             
+    price text );                
