cec498b47fdce54eeb18bfc3e29edd3d4eb0fed3
PranauvShanmuganathan <pranavsurya234@gmail.com>
2024-01-21 12:31:00 +0530

    refix  Menu table

--- ddl/menu.sql
+++ ddl/menu.sql
@@ -14,3 +14,28 @@ create or replace table menu (
     _stg_file_md5 text,
     _copy_data_ts timestamp default current_timestamp
 );
+
+
+copy into menu (menu_id, restaurant_id, item_name, description, price, category, 
+                availability, item_type, created_date, modified_date,
+                _stg_file_name, _stg_file_load_ts, _stg_file_md5, _copy_data_ts)
+from (
+    select 
+        t.$1::text as menuid,
+        t.$2::text as restaurantid,
+        t.$3::text as itemname,
+        t.$4::text as description,
+        t.$5::text as price,
+        t.$6::text as category,
+        t.$7::text as availability,
+        t.$8::text as itemtype,
+        t.$9::text as createddate,
+        t.$10::text as modifieddate,
+        metadata$filename as _stg_file_name,
+        metadata$file_last_modified as _stg_file_load_ts,
+        metadata$file_content_key as _stg_file_md5,
+        current_timestamp as _copy_data_ts
+    from @MOVE_IT_IN/Menu/ t
+)
+file_format = (format_name = 'csv_file_format')
+on_error = abort_statement;
